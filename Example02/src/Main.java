4649f0c0350bb206a46a8c744efa9f748ed810ca
Minh Đặng <Minh ng@MINHDANG>
2015-05-23 15:24:50 +0700

    222

---
+++ Example02/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		CRectangle recta = new CRectangle (3,4);
+		CRectangle rectb = new CRectangle (5,6);
+		
+		System.out.println("rect a:" + recta.area());
+		System.out.println("rect b:" + rectb.area());
+	}
+	}
